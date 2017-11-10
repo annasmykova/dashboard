ef2f2c9060f0ef94acaeb32c07318fdf1ed8d654
annasmykova <anna.smykova@nure.ua>
2017-11-10 03:52:05 +0200

    modals of banners and links

--- app/js/init.js
+++ app/js/init.js
@@ -37,11 +37,56 @@ $( function() {
     modalBtn.on('click', function (e) {
         e.preventDefault();
         var target = $(this).attr('data-target');
-        $(target).bPopup();
+        $(target).bPopup({
+            closeClass: 'close'
+        });
 
     })
 
     //formstyler
     $('select').styler();
+
+    //scroll-bar
+    $('.scrollbar-inner').scrollbar();
+
+    //chart init
+    var chartBlock = document.getElementById('myChart');
+    if( chartBlock){
+        var myChart = new Chart(chartBlock, {
+            type: 'bar',
+            data: {
+                labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09'],
+                datasets: [
+                    {
+                        label: 'Signups',
+                        data: [150 ,200 ,220, 150, 260, 250, 300, 540, 300],
+                        backgroundColor: 'rgba(128, 191, 202, 1)'
+                    },
+                    {
+                        label: 'FTD',
+                        data: [200 ,200 ,520, 200, 260, 450, 600, 440, 150],
+                        backgroundColor: 'rgba(255, 154, 56, 1)'
+                    },
+                    {
+                        label: 'FTD',
+                        data: [200 ,200 ,520, 200, 260, 450, 600, 440, 150],
+                        backgroundColor: '#59b66d'
+                    }
+
+                ]
+            },
+            options: {
+            responsive: true,
+                scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                    }]
+                }
+            }
+        })
+
+    }
 
 } );
